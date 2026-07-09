0f4760e9f7a30ac2a4adbfd800acee40fdc4ac2f
Christofanis Skordas <skordasc@uchicago.edu>
2026-07-08 22:53:23 -0500

    adds initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+    description = "Nix config";
+
+    inputs = {
+        # Nixpkgs
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+        # Flakes
+        flake-parts.url = "github:hercules-ci/flakes-parts";
+        import-tree.url = "github:denful/import-tree";
+
+        # Home manager
+        home-manager = {
+            url = "github:nix-community/home-manager/release-26.05";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        # MacOS
+        nix-darwin = {
+            url = "github:nix-darwin/nix-darwin";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        # General
+        hardware.url = "github:nixos/nixos-hardware";
+        nix-index-database = {
+            url = "github:nix-community/nix-index-database";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = {
+        inputs@{ flake-parts, import-tree, ... }:
+        flake-parts.lib.mkFlake { inherit inputs; } {
+            imports = [ (import-tree ./modules) ];
+
+            systems = [
+                "aarch64-darwin"
+                "x86_64-linux"
+            ];
+        };
+    };
+}
