be9d22c51f36bce6762f8a78802c941c36913822
Christofanis Skordas <skordasc@uchicago.edu>
2026-07-09 11:51:24 -0500

    adds initial modules

---
+++ modules/darwin-modules/base.nix
@@ -0,0 +1,130 @@
+{ ... }: 
+{
+    flake.darwinModules.base = { config, lib, pkgs, inputs, outputs, pkgs-unstable, ... }:
+    let
+        cfg = config.modules.base;
+        finderDownloadsPath = "file://localhost/Users/${config.system.primaryUser}/Downloads/";
+    in
+    {
+        imports = [ inputs.opnix.darwinModules.default ];
+        
+        options.modules.base = {
+            enable = lib.mkEnableOption "base darwin configuration" // {
+                default = true;
+            };
+        };
+
+        config = lib.mkIf cfg.enable {
+            ids.gids.nixbld = 30000;
+
+            nixpkgs = {
+                config = {
+                    allowUnfree = true;
+                    allowUnfreePredicate = _: true;
+                };
+        # Determinate Nix manages its own daemon and nix.conf. Disable nix-darwin's nix management to avoid conflicts.
+        nix.enable = false;
+
+        environment = {
+            systemPackages = [
+                pkgs.coreutils
+                pkgs.neovim
+                pkgs.home-manager
+                inputs.opnix.packages.${pkgs.stdenv.hostPlatform.system}.default
+            ];
+            shells = [
+                pkgs.bashInteractive
+            ];
+        };
+
+        programs = {
+            bash.enable = true;
+            zsh.enable = true;
+        };
+
+        fonts.packages = [
+            pkgs.monaspace
+            pkgs.nerd-fonts.monaspace
+            pkgs.nerd-fonts.symbols-only
+        ];
+
+        system.keyboard = {
+            enableKeyMapping = true;
+        };
+
+        system.defaults = {
+            menuExtraClock = {
+                FlashDateSeparators = true;
+                ShowDayOfWeek = true;
+                ShowDayOfMonth = true;
+                ShowAMPM = false;
+            };
+
+            WindowManager = {
+                EnableStandardClickToShowDesktop = false;
+            };
+
+            dock = {
+                autohide = true;
+                tilesize = 64;
+            };
+
+            finder = {
+                AppleShowAllExtensions = true;
+                FXEnableExtensionChangeWarning = false;
+                _FXShowPosixPathInTitle = false;
+                FXPreferredViewStyle = "Nlsv";
+                FXDefaultSearchScope = "SCcf";
+                ShowStatusBar = true;
+                ShowPathbar = true;
+            };
+
+            trackpad = {
+                Clicking = true;
+                TrackpadThreeFingerDrag = true;
+            };
+
+            SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+
+            CustomUserPreferences.NSGlobalDomain = {
+                AppleInterfaceStyle = "Dark";
+                "com.apple.swipescrolldirection" = false;
+                AppleShowAllExtensions = true;
+                NSGlassDiffusionSetting = 1;
+                ApplePressAndHoldEnabled = false;
+                InitialKeyRepeat = 15;
+                KeyRepeat = 2;
+                NSAutomaticCapitalizationEnabled = false;
+                NSAutomaticDashSubstitutionEnabled = false;
+                "com.apple.sound.beep.feedback" = 1;
+                NSNavPanelExpandedStateForSaveMode = true;
+                NSNavPanelExpandedStateForSaveMode2 = true;
+                PMPrintingExpandedStateForPrint = true;
+                PMPrintingExpandedStateForPrint2 = true;
+            };
+
+            CustomUserPreferences = {
+                "com.apple.finder" = {
+                    NewWindowTarget = "PfLo";
+                    NewWindowTargetPath = finderDownloadsPath;
+                };
+                "com.googlecode.iterm2" = {
+                    "PrefsCustomFolder" = "~/.config/iterm2";
+                    "LoadPrefsFromCustomFolder" = 1;
+                };
+                "com.apple.desktopservices".DSDontWriteNetworkStores = true;
+                "com.apple.desktopservices".DSDontWriteUSBStores = true;
+                "digital.twisted.noTunes" = {
+                    "hideIcon" = 1;
+                    "replacement" = "/Applications/Spotify.app";
+                };
+            };
+        };
+
+        security.pam.services.sudo_local.touchIdAuth = true;
+        security.sudo.extraConfig = ''
+            Defaults timestamp_timeout=30
+            '';
+        };
+    };
+}
