70a39e55475b112a73ca2a204de16363d55b8921
Christofanis Skordas <skordasc@uchicago.edu>
2026-07-10 14:38:43 -0500

    updates home-manager packages and shell aliases for vortex

--- modules/home-modules/vortex.nix
+++ modules/home-modules/vortex.nix
@@ -10,13 +10,20 @@
     })
   ];
 
+  home.packages = with pkgs; [
+    claude-code
+    firefox
+  ];
+
   programs.zsh = {
       enable = true;
       shellAliases = {
+          cat = "bat";
+          ll = "eza -lah --group-directories-first --sort=name";
+          ls = "eza";
+          nix-rebuild = "sudo -H nix run nix-darwin/nix-darwin-26.05#darwin-rebuild -- switch --flake ~/Repos/dotfiles#vortex --impure";
           sudo = "sudo -E";
           vim = "nvim";
-          cat = "bat";
-          nix-rebuild = "sudo -H nix run nix-darwin/nix-darwin-26.05#darwin-rebuild -- switch --flake ~/Repos/dotfiles#vortex --impure";
       };
   };
 }
