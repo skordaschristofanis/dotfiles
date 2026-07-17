4836a788a2788a579b34f891e88cd7120b3e31d9
Christofanis Skordas <skordasc@uchicago.edu>
2026-07-16 21:37:25 -0500

    adds some dev packages and lib path

--- modules/home-modules/hellios.nix
+++ modules/home-modules/hellios.nix
@@ -18,5 +18,11 @@
       cat = "bat";
       nix-rebuild = "sudo nixos-rebuild switch --flake /repos/dotfiles#hellios --impure";
     };
+    initExtra = ''
+      export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
+        pkgs.stdenv.cc.cc.lib
+        pkgs.zlib
+      ]}:''${LD_LIBRARY_PATH:-}"
+    '';
   };
 }
